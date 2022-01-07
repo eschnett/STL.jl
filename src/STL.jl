b508faeefcd608913c221b570e46744dfb1588dc
Erik Schnetter <schnetter@gmail.com>
2022-01-07 09:50:18 -0500

    Run self-tests at load time

--- src/STL.jl
+++ src/STL.jl
@@ -4,16 +4,6 @@ using CxxInterface
 using STL_jll
 using TestAbstractTypes
 
-eval(cxxsetup())
-
-include("Defs.jl")
-include("Types.jl")
-
-include("StdMap.jl")
-include("StdSharedPtr.jl")
-include("StdString.jl")
-include("StdVector.jl")
-
 # Run all self-tests when the package is loaded
 const selftests = Any[]
 function __init__()
@@ -24,5 +14,15 @@ function __init__()
         end
     end
 end
+
+eval(cxxsetup())
+
+include("Defs.jl")
+include("Types.jl")
+
+include("StdMap.jl")
+include("StdSharedPtr.jl")
+include("StdString.jl")
+include("StdVector.jl")
 
 end
